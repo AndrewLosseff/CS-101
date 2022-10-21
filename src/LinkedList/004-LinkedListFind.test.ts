58046f278d73e0fdd76b6e77b9a47cb470a6be1d
AndrewLosseff <AndrewLosseff@gmail.com>
2022-10-21 14:38:52 +0300

    add find node

---
+++ src/LinkedList/004-LinkedListFind.test.ts
@@ -0,0 +1,29 @@
+// To run test use this:
+// yarn jest src/LinkedList/004-LinkedListFind --watch
+
+import { linkedListMock } from './mock'
+import { linkedListFind, linkedListRecursiveFind } from './004-LinkedListFind'
+
+test('Linked List Find', () => {
+    expect(linkedListFind).toBeDefined();
+});
+
+test('linkedListFind, linkedListMock', () => {
+    expect(linkedListFind(linkedListMock, 5)).toEqual(true)
+});
+
+test('linkedListFind, linkedListMock', () => {
+    expect(linkedListFind(linkedListMock, 4)).toEqual(false)
+});
+
+test('Linked List Recursive Find', () => {
+    expect(linkedListRecursiveFind).toBeDefined();
+});
+
+test('linkedListFind, linkedListMock', () => {
+    expect(linkedListRecursiveFind(linkedListMock, 5)).toEqual(true)
+});
+
+test('linkedListFind, linkedListMock', () => {
+    expect(linkedListRecursiveFind(linkedListMock, 4)).toEqual(false)
+});
