8e22e2161a784dcb33599bcf60499825120922cd
AndrewLosseff <AndrewLosseff@gmail.com>
2022-10-23 12:41:29 +0300

    add zipper problem

--- src/LinkedList/mock.ts
+++ src/LinkedList/mock.ts
@@ -29,4 +29,53 @@ export const linkedListRevesedMock: ListNode = {
     },
 }
 
+export const zipperResultsFirst: ListNode = {
+    val: 5,
+    next: {
+        val: 10,
+        next: {
+            val: 3,
+            next: {
+                val: 3,
+                next: {
+                    val: 10,
+                    next: {
+                        val: 5,
+                        next: null,
+                    }
+                }
+            }
+        }
+    }
+}
+
+export const shortList: ListNode = {
+    val: 99,
+    next: null
+}
+
+export const zipperResultsSecons: ListNode = {
+    val: 5,
+    next: {
+        val: 99,
+        next: {
+            val: 10,
+            next: {
+                val: 3,
+                next: {
+                    val: 3,
+                    next: {
+                        val: 10,
+                        next: {
+                            val: 5,
+                            next: null,
+                        }
+                    }
+                }
+            } 
+        }
+    }
+}
+
 export const emptyArray = []
+
